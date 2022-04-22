7e6ea3cdab9aeb47eb4374ddb84bea66616f7c76
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-22 10:48:37 +0200

    Add project files.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        private string titolo;
+        private DateTime data;
+        private int capienzaMassima;
+        private int prenotazioni;
+
+
+
+
+        //getter - setter
+
+        //titolo: lettura e scrittura
+
+        //data: lettura e scrittura
+
+
+
+        //capienzaMax: solo lettura
+
+        public int GetCapienzaMassima()
+        { return capienzaMassima; }
+
+        //prenotazioni: solo lettura;
+
+        public int GetPrenotazioni()
+        { return prenotazioni; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
